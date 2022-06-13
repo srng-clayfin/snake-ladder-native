42db6651498a5c8b94f0a9d533eb6decf602eae7
Sarang Gunjal <sarang@clayfin.com>
2022-06-13 10:46:51 +0530

    ...

--- snake-ladder-game/components/Stack.jsx
+++ snake-ladder-game/components/Stack.jsx
@@ -1,4 +1,5 @@
 import { createStackNavigator, Header } from '@react-navigation/stack';
+import { Button } from 'react-native';
 import { Game } from './Game';
 import { Main } from './Main';
 import { UserInformation } from './UserInformation';
@@ -10,7 +11,16 @@ export const MyStack = () => {
         <Stack.Navigator>
             <Stack.Screen name="Main" component={Main} />
             <Stack.Screen name="Userinformation" component={UserInformation} />
-            <Stack.Screen name="Game" component={Game} />            
+            <Stack.Screen name="Game" component={Game} options={({ navigation, route }) => ({
+                headerLeft: (props) => (
+                    <Button
+                        {...props}
+                        title="Restart"
+                        onPress={() => navigation.navigate('Main')}
+                    />
+                ),
+            })} />            
+            <Stack.Screen name="Playername" component={null} />
         </Stack.Navigator>
     );
 }
